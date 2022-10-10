39c289a43034f6eb7601f0cbf980d6b50ad8c191
Zeshan Khatri <zeshan.khatri@gmail.com>
2022-10-09 23:09:49 -0400

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import requests
+
+st.set_page_config(
+    page_title = "Topify",
+    layout="wide",
+    menu_items = {
+        'Get Help' : 'https://docs.streamlit.io/',
+        'About' : '# Developed by Julio Arroyo and Zeshan Khatri'
+    }
+)
+
+st.write("Hello. Testing.")

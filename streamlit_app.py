5e495e987ffda8a8f8bd06fa9300067a13331609
Zeshan Khatri <zeshan.khatri@gmail.com>
2022-10-10 01:08:12 -0400

    first spotify api test

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ if sliders:
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
     results = sp.current_user_saved_tracks()
+    st.success("It works!")
     for idx, item in enumerate(results['items']):
         track = item['track']
         st.write(f"{idx + 1} {track['artists'][0]['name']} - {track['name']}")

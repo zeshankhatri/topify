7cfca8038cfd8e806c1f64f72ec5c12d2b1662dc
Zeshan Khatri <zeshan.khatri@gmail.com>
2022-10-11 16:21:51 -0400

    Checking HTML in Streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,21 +75,70 @@ if add_selectbox == "Top Artist":
 elif add_selectbox == "Spotify's Available Markets":
     st.write("In progress")
 else:
-    login = st.button("Login to Spotify")
+    # login = st.button("Login to Spotify")
+    #
+    # if login:
+    #     webbrowser.open_new_tab(auth_url)
+    link_html = "<style> "
+    link_html += "  button {"
+    link_html += "    display: inline-block;"
+    link_html += "    background-color: #7b38d8;"
+    link_html += "    border-radius: 10px;"
+    link_html += "    border: 4px double #cccccc;"
+    link_html += "    color: #eeeeee;"
+    link_html += "    text-align: center;"
+    link_html += "    font-size: 28px;"
+    link_html += "    padding: 20px;"
+    link_html += "    width: 200px;"
+    link_html += "    transition: all 0.5s;"
+    link_html += "    cursor: pointer;"
+    link_html += "    margin: 5px;"
+    link_html += "  }"
+    link_html += "  button span {"
+    link_html += "    cursor: pointer;"
+    link_html += "    display: inline-block;"
+    link_html += "    position: relative;"
+    link_html += "    transition: 0.5s;"
+    link_html += "  }"
+    link_html += "  button span:after {"
+    link_html += "    content: '\00bb';"
+    link_html += "    position: absolute;"
+    link_html += "    opacity: 0;"
+    link_html += "    top: 0;"
+    link_html += "    right: -20px;"
+    link_html += "    transition: 0.5s;"
+    link_html += "  }"
+    link_html += "  button:hover {"
+    link_html += "    background-color: #f7c2f9;"
+    link_html += "  }"
+    link_html += "  button:hover span {"
+    link_html += "    padding-right: 25px;"
+    link_html += "  }"
+    link_html += "  button:hover span:after {"
+    link_html += "    opacity: 1;"
+    link_html += "    right: 0;"
+    link_html += "  }"
+    link_html += "</style>"
+    link_html = " <a href=\"{url}\" > ".format(
+        url=auth_url,
+    )
+    link_html += "<button>"
+    link_html += "  <span>Submit</span>"
+    link_html += "</button>"
+    link_html += "</a>"
 
-    if login:
-        webbrowser.open_new_tab(auth_url)
+    st.markdown(link_html, unsafe_allow_html=True)
 
 sliders = st.checkbox("Test Me!")
 
 if sliders:
 
-    link_html = " <a href=\"{url}\" >{msg}</a> ".format(
-        url=auth_url,
-        msg="Click me to authenticate!"
-    )
+    # link_html = " <a href=\"{url}\" >{msg}</a> ".format(
+    #     url=auth_url,
+    #     msg="Click me to authenticate!"
+    # )
 
-    st.markdown(link_html, unsafe_allow_html=True)
+    # st.markdown(link_html, unsafe_allow_html=True)
 
     # If code not in parameters, error is thrown
     if 'code' not in url_params:

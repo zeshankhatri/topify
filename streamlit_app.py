2c83c8f76d52be3166da029ad2a50a3bc9680c96
Zeshan Khatri <zeshan.khatri@gmail.com>
2022-10-11 00:07:31 -0400

    First test of top tracks API in short term.

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,4 +88,4 @@ if sliders:
         for idx, item in enumerate(results['items']):
             track = item['name']
             artist = item['artists'][0]['name']
-            st.text("%i\t%-50s %-50s" % (idx+1, track, artist))
+            st.text("%i\t%-47s %-50s" % (idx+1, track, artist))

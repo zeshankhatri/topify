57aa449b2a476b3d939344e00ba97a1ba55f0e9e
Zeshan Khatri <zeshan.khatri@gmail.com>
2022-10-11 23:20:20 -0400

    Created login button using HTML and CSS styling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ add_selectbox = st.sidebar.selectbox(
     ["Top Tracks", "Top Artists", "Spotify's Available Markets"]
 )
 
-if add_selectbox == "Top Artist":
+if add_selectbox == "Top Artists":
     st.write("In progress")
 elif add_selectbox == "Spotify's Available Markets":
     st.write("In progress")
@@ -80,70 +80,17 @@ else:
     # if login:
     #     webbrowser.open_new_tab(auth_url)
     link_html = "<style> "
-    link_html += "  button {"
-    link_html += "    display: inline-block;"
-    link_html += "    background-color: #7b38d8;"
-    link_html += "    border-radius: 10px;"
-    link_html += "    border: 4px double #cccccc;"
-    link_html += "    color: #eeeeee;"
-    link_html += "    text-align: center;"
-    link_html += "    font-size: 28px;"
-    link_html += "    padding: 20px;"
-    link_html += "    width: 200px;"
-    link_html += "    transition: all 0.5s;"
-    link_html += "    cursor: pointer;"
-    link_html += "    margin: 5px;"
-    link_html += "  }"
-    link_html += "  button span {"
-    link_html += "    cursor: pointer;"
-    link_html += "    display: inline-block;"
-    link_html += "    position: relative;"
-    link_html += "    transition: 0.5s;"
-    link_html += "  }"
-    link_html += "  button span:after {"
-    link_html += "    content: '\00bb';"
-    link_html += "    position: absolute;"
-    link_html += "    opacity: 0;"
-    link_html += "    top: 0;"
-    link_html += "    right: -20px;"
-    link_html += "    transition: 0.5s;"
-    link_html += "  }"
-    link_html += "  button:hover {"
-    link_html += "    background-color: #f7c2f9;"
-    link_html += "  }"
-    link_html += "  button:hover span {"
-    link_html += "    padding-right: 25px;"
-    link_html += "  }"
-    link_html += "  button:hover span:after {"
-    link_html += "    opacity: 1;"
-    link_html += "    right: 0;"
-    link_html += "  }"
+    link_html += " button { display: inline-block; background-color: #1db954; border-radius: 10px; border: 4px single #cccccc; color: #eeeeee;"
+    link_html += " text-align: center; font-size: 16px; padding: 10px; transition: all 0.5s; cursor: pointer; margin-bottom: 10px; }"
+    link_html += " button span { cursor: pointer; display: inline-block; position: relative; transition: 0.5s; }"
+    link_html += " button span:after { content: '>>'; position: absolute; opacity: 0; top: 0; right: -20px; transition: 0.5s; }"
+    link_html += " button:hover span { padding-right: 25px; }"
+    link_html += " button:hover span:after { opacity: 1; right: 0; }"
     link_html += "</style>"
-    link_html += " <a href=\"{url}\" > ".format(
-        url=auth_url,
-    )
-    link_html += "<button>"
-    link_html += "  <span>Submit</span>"
-    link_html += "</button>"
-    link_html += "</a>"
+    link_html += f" <a href=\"{auth_url}\" > <button> <span>Login to Spotify</span> </button> </a>"
 
-    st.markdown(link_html, unsafe_allow_html=True)
-    st.text(link_html)
-
-sliders = st.checkbox("Test Me!")
-
-if sliders:
-
-    # link_html = " <a href=\"{url}\" >{msg}</a> ".format(
-    #     url=auth_url,
-    #     msg="Click me to authenticate!"
-    # )
-
-    # st.markdown(link_html, unsafe_allow_html=True)
-
-    # If code not in parameters, error is thrown
-    if 'code' not in url_params:
-        st.error("Please Reauthenticate")
+    if code not in url_params:
+        st.markdown(link_html, unsafe_allow_html=True)
     else:
         # Get user data
         code = url_params['code'][0]
@@ -168,4 +115,4 @@ if sliders:
         for idx, item in enumerate(results['items']):
             track = item['name']
             artist = item['artists'][0]['name']
-            st.text("%i\t%-47s %-50s" % (idx+1, track, artist)) # st.text used as st.write doesn't support \t
+            st.text("%i\t%-47s %-50s" % (idx + 1, track, artist))  # st.text used as st.write doesn't support \t
